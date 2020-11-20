da36add2687a2fc794e08c5e10610a1d4e0e4401
ctrlaltf2 <ctrlaltf2@protonmail.ch>
2020-11-20 17:59:20 -0500

    Wawa Update - Add sitemap-based scraper
    Keeps getting blocked, ran out of IPs to test for now. For some reason, Wawa's site is very sensitive to non-browser access.

--- locations/spiders/wawa.py
+++ locations/spiders/wawa.py
@@ -1,67 +1,83 @@
 import json
+import re
 import scrapy
 from six.moves.urllib.parse import urlencode
 from locations.items import GeojsonPointItem
 
-LAT_LONS = [
-    # Northeast locations
-    ('37.7666545', '-74.8088835'),
-    ('37.21232025', '-76.47188625'),
-    ('37.21232025', '-77.58055475'),
-    ('38.32098875', '-76.47188625'),
-    ('38.32098875', '-77.58055475'),
-    ('39.152490125', '-74.531716375'),
-    ('39.706824375', '-73.977382125'),
-    ('39.706824375', '-74.531716375'),
-    ('39.152490125', '-75.0860506250'),
-    ('39.152490125', '-75.640384875'),
-    ('39.5682408125', '-74.9474670625'),
-    ('39.5682408125', '-75.2246341875'),
-    ('39.8454079375', '-74.9474670625'),
-    ('39.8454079375', '-75.2246341875'),
-    ('39.706824375', '-75.640384875'),
-    ('40.53832575', '-74.25454925'),
-    ('40.1225750625', '-74.9474670625'),
-    ('40.1225750625', '-75.2246341875'),
-    ('40.3997421875', '-74.9474670625'),
-    ('40.3997421875', '-75.2246341875'),
-    ('40.261158625', '-75.640384875'),
-    ('40.815492875', '-75.0860506250'),
-    ('40.815492875', '-75.640384875'),
-    ('39.9839915', '-77.0262205'),
-    ('39.9888', '-75.4026'),
-    ('39.984', '-75.101'),
-    ('40.128', '-74.707'),
-    # Florida Locations
-    ('25.9939', '-80.2966'), # Most southern store location
-    ('27.4473', '-82.5758'), # Sarasota area locations
-    ('28.0692', '-82.4306'), # Tampa North area locations
-    ('28.2288', '-81.6487'), # Orlando area locations
-    ('30.2704', '-81.7560'), # Jacksonville area locations
-]
-
-
 class WawaSpider(scrapy.Spider):
     name = 'wawa'
     item_attributes = { 'brand': "Wawa" }
-    start_urls = (
-        'https://www.wawa.com/Handlers/LocationByLatLong.ashx?',
-    )
-    download_delay = 1.5
-    allowed_domains = ('www.wawa.com')
+    download_delay = 4
+    allowed_domains = ['www.wawa.com']
 
     def start_requests(self):
-        url = 'https://www.wawa.com/Handlers/LocationByLatLong.ashx?'
-        for lat, lon in LAT_LONS:
-            wawa_params = {'limit': 50, 'lat': lat, 'long': lon}
+        """ Initial request: get the list of valid store numbers through a sitemap xml """
+        yield scrapy.Request('https://www.wawa.com/search-sitemaps/store-sitemap',
+                             callback=self.process_sitemap,
+                             method='GET',
+                             headers = { 'Host': 'www.wawa.com' },
+                             cookies = {'reese84': '3:dxpwQ5yByy9PqWsb714GBA==:hAwjbzbzIKd6d49Ipk9bvsE/pRsmNkAjX1OGKPkepY+omEu2G7B0mr6jWCLwmCKCI8CezFEbvmolg+Byrl9rqaM/UqfF/jOMNk2yrXnhMnFjIO5HV0ojYlNEOcshs/oJKHFbYTo4M3zRnEUCx48yuCHKJaHUyYJIwW57D0HgsaSjxYJ9zKwbDBEYwO1/UZ+gZ7yOqAOyi1pTuZHpf8wDp9Vy1xGBsGV13E688OHW7Mdts8tCqxwzGuU9WthEsV3ENcwGkd2L6jqDa0jw7+OVj+9Mb7Ys5GRBCPaaKqD+rHB+qKjVdWyLpKUKoxXSFEOKQw9V57bcESYS/0JNnQYmAHshlSP1aqVRsRlfQm7kKU9S5SvJC3o5KUif8q5vTODSvlAlwAHufMsHJCUgVKqP88pLiXCHrBVRiwV+N4RXf+E=:ZSA0utWM5scQgXHEHWVzkciEmf3hzs8CrYRTJ8BVo8Q='}
+                            )
 
+    def process_sitemap(self, response):
+        response.selector.remove_namespaces()
+        store_urls = response.xpath('//urlset/url/loc/text()').extract()
+        print(store_urls)
+
+        for store_url in store_urls:
+            print('processing', store_url)
+            # Store urls look like: https://www.wawa.com/Stores/<STORE NUMBER>/...
+            store_num = re.search(r'https://www\.wawa\.com/Stores/(\d+)/', store_url).group(1)
+
+            wawa_params = {'storeNumber': store_num}
             yield scrapy.Request(
-                url + urlencode(wawa_params),
+                'https://www.wawa.com/Handlers/LocationByStoreNumber.ashx?' + urlencode(wawa_params),
                 headers={
                     'Accept': 'application/json',
+                    'Host': 'www.wawa.com'
                 },
+                cookies = {'reese84': '3:dxpwQ5yByy9PqWsb714GBA==:hAwjbzbzIKd6d49Ipk9bvsE/pRsmNkAjX1OGKPkepY+omEu2G7B0mr6jWCLwmCKCI8CezFEbvmolg+Byrl9rqaM/UqfF/jOMNk2yrXnhMnFjIO5HV0ojYlNEOcshs/oJKHFbYTo4M3zRnEUCx48yuCHKJaHUyYJIwW57D0HgsaSjxYJ9zKwbDBEYwO1/UZ+gZ7yOqAOyi1pTuZHpf8wDp9Vy1xGBsGV13E688OHW7Mdts8tCqxwzGuU9WthEsV3ENcwGkd2L6jqDa0jw7+OVj+9Mb7Ys5GRBCPaaKqD+rHB+qKjVdWyLpKUKoxXSFEOKQw9V57bcESYS/0JNnQYmAHshlSP1aqVRsRlfQm7kKU9S5SvJC3o5KUif8q5vTODSvlAlwAHufMsHJCUgVKqP88pLiXCHrBVRiwV+N4RXf+E=:ZSA0utWM5scQgXHEHWVzkciEmf3hzs8CrYRTJ8BVo8Q='},
                 callback=self.parse
             )
+
+    def parse(self, response):
+        print('parse')
+        print(response.text)
+        loc = json.loads(response.text)
+
+        addr, city, state, zipc = self.get_addr(loc['addresses'][0])
+        lat, lng = self.get_lat_lng(loc['addresses'][1])
+        opening_hours = self.get_opening_hours(loc) or None
+
+        properties = {
+            'addr_full': addr,
+            'name': loc['storeName'],
+            'phone': loc['telephone'],
+            'city': city,
+            'state': state,
+            'postcode': zipc,
+            'ref': loc['locationID'],
+            'website': "https://www.wawa.com/stores/{}/{}".format(
+                loc['locationID'],
+                loc['addressUrl'],
+            ),
+            'lat': float(lat),
+            'lon': float(lng),
+            'opening_hours': opening_hours,
+            'extras': {
+                'food': loc['amenities']['food'],
+                'fuel': loc['amenities']['fuel'],
+                'fuel_types': self.get_fuel_types(loc['fuelTypes']),
+                'restrooms': loc['amenities']['restrooms'],
+                'ethanol_free_gas': loc['amenities']['ethanolFreeGas'],
+                'tesla_charging_station': loc['amenities']['teslaChargingStation'],
+                'propane_exchange': loc['amenities']['propane'],
+            }
+        }
+
+        print('parsed', properties)
+
+        yield GeojsonPointItem(**properties)
 
     def get_addr(self, addr):
         return (v for k, v in addr.items() if k in ['address', 'city', 'state', 'zip'])
@@ -80,30 +96,9 @@ class WawaSpider(scrapy.Spider):
         else:
             return times
 
-    def parse(self, response):
-        wawa_stores = json.loads(response.body_as_unicode())
+    def get_fuel_types(self, fuel_types):
+        types = []
+        for fuel in fuel_types:
+            types.append(fuel['description'].lower())
 
-        for loc in wawa_stores['locations']:
-
-            addr, city, state, zipc = self.get_addr(loc['addresses'][0])
-            lat, lng = self.get_lat_lng(loc['addresses'][1])
-            opening_hours = self.get_opening_hours(loc) or None
-
-            properties = {
-                'addr_full': addr,
-                'name': loc['storeName'],
-                'phone': loc['telephone'],
-                'city': city,
-                'state': state,
-                'postcode': zipc,
-                'ref': loc['locationID'],
-                'website': "https://www.wawa.com/stores/{}/{}".format(
-                    loc['locationID'],
-                    loc['addressUrl'],
-                ),
-                'lat': float(lat),
-                'lon': float(lng),
-                'opening_hours': opening_hours
-            }
-
-            yield GeojsonPointItem(**properties)
+        return ';'.join(types)
